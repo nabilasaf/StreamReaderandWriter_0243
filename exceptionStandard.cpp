049564d57e45ac27baea7b3890c38849dacdf57c
nabilasaf <nabilasafitri861@gmail.com>
2024-06-04 15:10:00 +0700

    Awal Program

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -3,3 +3,21 @@
 #include <array>
 using namespace std;
 
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try{
+        array<int, 3> data = {3, 5, 7};
+        //pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        //penangkapan menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program yang Terakhir" << endl;
+    /*penanda 2: bahwa progra berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
+}
